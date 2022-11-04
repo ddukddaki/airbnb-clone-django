fbee9f4f5c58b76f364d49b1ab2df81d866f9c25
Giheon <74302278+Dockerel@users.noreply.github.com>
2022-11-04 16:17:56 +0900

    8.3 code challenge

--- reviews/admin.py
+++ reviews/admin.py
@@ -2,6 +2,43 @@ from django.contrib import admin
 from .models import Review
 
 
+class WordFilter(admin.SimpleListFilter):
+
+    title = "Filter by words!"
+
+    parameter_name = "target_word"
+
+    def lookups(self, request, model_admin):
+        return [("wow", "Wow"), ("god", "God")]
+
+    def queryset(self, request, reviews):
+        word = self.value()
+        if word:
+            return reviews.filter(payload__contains=word)
+        else:
+            reviews
+
+
+class ReviewEvaluationFilter(admin.SimpleListFilter):
+
+    title = "Filter good and bad reviews."
+
+    parameter_name = "evaluation_status"
+
+    def lookups(self, request, model_admin):
+        return [("good", "Good"), ("bad", "Bad")]
+
+    def queryset(self, request, reviews):
+        status = self.value()
+        if status:
+            if status == "good":
+                return reviews.filter(rating__gte=3)
+            else:
+                return reviews.filter(rating__lt=3)
+        else:
+            return reviews
+
+
 @admin.register(Review)
 class ReviewAdmin(admin.ModelAdmin):
 
@@ -9,4 +46,11 @@ class ReviewAdmin(admin.ModelAdmin):
         "__str__",
         "payload",
     )
-    list_filter = ("rating",)
+    list_filter = (
+        ReviewEvaluationFilter,
+        WordFilter,
+        "rating",
+        "user__is_host",
+        "room__category",
+        "room__pet_friendly",
+    )

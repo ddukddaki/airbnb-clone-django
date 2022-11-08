d83be5f7a678159f1af1f6372a62e53b7ecf6436
Giheon <74302278+Dockerel@users.noreply.github.com>
2022-11-08 22:06:34 +0900

    Django Rest Framework prac : ~update()

--- categories/views.py
+++ categories/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.exceptions import NotFound
+from rest_framework.response import Response
+from .models import Category
+from .serializers import CategorySerializer
 
-# Create your views here.
+
+@api_view(["GET", "POST"])
+def categories(request):
+
+    if request.method == "GET":
+        all_categories = Category.objects.all()
+        serializer = CategorySerializer(all_categories, many=True)
+        return Response(serializer.data)
+    elif request.method == "POST":
+        serializer = CategorySerializer(data=request.data)  # Data from User, Dato to Django
+        if serializer.is_valid():
+            new_category = serializer.save()
+            return Response(CategorySerializer(new_category).data)
+        else:
+            return Response(serializer.errors)
+
+
+@api_view(["GET", "PUT"])
+def category(request, pk):
+    try:
+        category = Category.objects.get(pk=pk)
+    except Category.DoesNotExist:
+        raise NotFound
+    if request.method == "GET":
+        serealizer = CategorySerializer(category)  # Django to JSON
+        return Response(serealizer.data)
+    elif request.method == "PUT":
+        serealizer = CategorySerializer(
+            category,
+            data=request.data,
+            partial=True,
+        )
+        if serealizer.is_valid():
+            updated_category = serealizer.save()
+            return Response(CategorySerializer(updated_category).data)
+        else:
+            return Response(serealizer.errors)

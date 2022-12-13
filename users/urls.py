176ccf2bafeafad47b15eeb1f2b75e79e7e4e39b
Giheon <74302278+Dockerel@users.noreply.github.com>
2022-12-13 21:52:41 +0900

    github login finished

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,16 @@
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
-from .views import Me, Users, PublicUser, ChangePassword, LogIn, LogOut, PublicUserReviews, JWTLogIn
+from .views import (
+    Me,
+    Users,
+    PublicUser,
+    ChangePassword,
+    LogIn,
+    LogOut,
+    PublicUserReviews,
+    JWTLogIn,
+    GithubLogIn,
+)
 
 urlpatterns = [
     path("", Users.as_view()),
@@ -10,6 +20,7 @@ urlpatterns = [
     path("log-out", LogOut.as_view()),
     path("token-login", obtain_auth_token),
     path("jwt-login", JWTLogIn.as_view()),
+    path("github", GithubLogIn.as_view()),
     path("@<str:username>", PublicUser.as_view()),
     path("@<str:username>/reviews", PublicUserReviews.as_view()),
 ]

5caf6f2acfa0a301bbeae68a2923c60722caf092
Giheon <74302278+Dockerel@users.noreply.github.com>
2022-12-26 20:54:36 +0900

    lock file changed

--- build.sh
+++ build.sh
@@ -2,8 +2,7 @@
 # exit on error
 set -o errexit
 
-pip install --upgrade pip
 poetry install
-pip install --force-reinstall -U setuptools
+
 python manage.py collectstatic --no-input
 python manage.py migrate

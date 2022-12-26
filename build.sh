4bd99e9b259dca49ac240d6fe41cfedcf955e28b
Giheon <74302278+Dockerel@users.noreply.github.com>
2022-12-26 19:55:58 +0900

    build.sh changed

--- build.sh
+++ build.sh
@@ -2,7 +2,8 @@
 # exit on error
 set -o errexit
 
+pip install --upgrade pip
 poetry install
-
+pip install --force-reinstall -U setuptools
 python manage.py collectstatic --no-input
 python manage.py migrate
